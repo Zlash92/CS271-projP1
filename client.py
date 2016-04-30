6a054726b6ab72e12f7a2fbdf34322445e5b1323
Manu Gopinathan <manugopi92@gmail.com>
2016-04-29 18:26:32 -0700

    fixing lookup... need json

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import socket
+import replicated
 
 
 class Client:
@@ -13,30 +15,25 @@ class Client:
       s.connect((host, port))
       connection = True
     except:
-      print 'Unable to connect to server'
+      print('Unable to connect to server')
       s.close()
 
     if connection:
       # Receive no more than 1024 bytes
-      print self.s.recv(1024)
+      print(self.s.recv(1024))
 
       while True:
         msg = raw_input('Enter message: ')
         if msg == 'close':
           self.s.send(msg)
           break
+        if msg == 'lookup':
+          self.s.send(msg)
+          recv = s.recv(1024)
+          
         self.s.send(msg)
       self.s.close()
 
 
-  def post(self, msg):
-    pass
-
-  def lookup(self):
-    pass
-
-  def sync(self, other):
-    pass
-
-
-c = Client(host='128.111.84.221')
+# c = Client(host='128.111.84.221')
+c = Client(host=socket.gethostname(), port=18861)
