427b77730fa8d2e2e74ff6bdb7edc476462d8469
Manu Gopinathan <manugopi92@gmail.com>
2016-04-30 17:31:56 -0700

    took care of a lot of shit

--- server.py
+++ server.py
@@ -1,62 +1,85 @@
 from __future__ import print_function
 import socket
 import replicated
+from time_table import TimeTable
+import sys
+import threading
+import pickle
 
 
 class Server:
+    def __init__(self, serverId=sys.argv[1], port=80):
+        self.socket = socket.socket()
+        self.host = socket.gethostname()
+        self.socket.bind((self.host, port))
+        self.data = replicated.ReplicatedDictionary()
+        self.serverId = serverId
+        self.timeTable = TimeTable(self.serverId, 3)
+        self.threads = []
+
+        self.initConnection()
+
+    def initConnection(self):
+        self.socket.listen(5)
+        print("Server with id=", self.serverId, " is running and listening for incoming connections", sep="")
+
+        while True:
+            c, addr = self.socket.accept()
+            print("Connected to", addr)
+            c.send("Connection to server was successful")
+            client = ClientHandler(c, addr, self)
+            client.start()
+            self.threads.append(client)
+
+        self.socket.close()
+        for client in self.threads:
+            client.join()
+
+    def post(self, msg, author):
+        self.incrementTime()
+        self.data.addPost(self.timeTable.get_self_clock(), msg, author)
+        print("Post has been submitted at local time", self.timeTable.get_self_clock())
+
+    def lookup(self, c):
+        package = pickle.dumps(self.data)
+        c.send(package)
+
+    def sync(self, other):
+        pass
+
+    def incrementTime(self):
+        self.timeTable.increment_self()
+
+    def listen(self):
+        self.socket.listen(5)
+        print("Server with id=", self.serverId, " is running and listening for incoming connections", sep="")
 
 
-  def __init__(self, port=80):
-    self.socket = socket.socket()
-    self.host = socket.gethostname()
-    self.socket.bind((self.host, port))
-    self.data = replicated.ReplicatedDictionary()
-    self.time = 0
-    self.timeTable = None
 
-    self.initConnection()
+class ClientHandler(threading.Thread):
+    def __init__(self, client, address, parentServer):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.parentServer = parentServer
+
+    def run(self):
+        while True:
+            recv = self.client.recv(1024)
+            inp = recv.split(' ', 1)
+            if recv == 'close':
+                print("Closing client connection")
+                self.client.close()
+                break
+            elif inp[0] == 'post':
+                self.parentServer.post(inp[1], self.address)
+            elif inp[0] == 'lookup':
+                self.parentServer.lookup(self.client)
+            elif inp[0] == 'sync':
+                self.parentServer.sync()
+            else:
+                print("Received message:", recv)
 
 
-  def initConnection(self):
-    self.socket.listen(5)
-    print("Server is running and listening for incoming connections")
-
-    while True:
-      c, addr = self.socket.accept()
-      print("Connected to", addr)
-      c.send("Connection to server was successful")
-
-      while True:
-        recv = c.recv(1024)
-        inp = recv.split(' ', 1)
-        if recv == 'close':
-          print("Closing client connection")
-          c.close()
-          break
-        elif inp[0] == 'post':
-          self.post(inp[1], addr)
-        elif inp[0] == 'lookup':
-          self.lookup(c)
-        elif inp[0] == 'sync':
-          self.sync()
-        else:
-          print("Received message:", recv)
-
-      break
-
-  def post(self, msg, author):
-    self.incrementTime()
-    self.data.addPost(self.time, msg, author)
-    print("Post has been submitted at local time", self.time)
-
-  def lookup(self, c):
-    c.send(self.data)
-
-  def sync(self, other):
-    pass
-
-  def incrementTime(self):
-    self.time += 1
-
-server = Server(port=18861)
+server = Server(serverId=0, port=18869)
 # Server()
--- time_table.py
+++ time_table.py
@@ -1,9 +1,10 @@
 class TimeTable(object):
 
-    def __init__(self, size):
+    def __init__(self, serverId, size):
 
         self.size = size
         self.table = [[0 for _ in range(size)] for _ in range(size)]
+        self.serverId = serverId
 
     def sync_tables(self, table2, self_id, table2_id):
 
@@ -17,5 +18,11 @@ class TimeTable(object):
     def update(self, server, count):
         self.table[server][server] = count
 
+    def increment_self(self):
+        self.table[self.serverId][self.serverId] += 1
+
     def get_table_entry(self, i, j):
         return self.table[i][j]
+
+    def get_self_clock(self):
+        return self.table[self.serverId][self.serverId]
